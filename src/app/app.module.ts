6d3efb1e91469d440663c486e44a0db00cab2832
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-06 16:37:47 -0500

    feat: project created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,68 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DoctorTableComponent } from './countability/components/doctor-table/doctor-table.component';
+
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { DetailPageComponent } from './countability/pages/detail-page/detail-page.component';
+import {MatSort} from '@angular/material/sort';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatButtonToggle} from '@angular/material/button-toggle';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatButton} from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInput} from '@angular/material/input';
+
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorTableComponent,
+    DetailPageComponent
   ],
   imports: [
+    MatAutocompleteModule,
+    MatProgressSpinnerModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatSort,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatPaginator,
+    MatButtonToggle,
+    MatFormField,
+    MatButton,
+    ReactiveFormsModule,
+    MatInput,
+    MatError,
+    MatLabel,
+    MatPaginatorModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
